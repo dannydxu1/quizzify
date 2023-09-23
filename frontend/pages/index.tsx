d8a44f762ee64f2f2deb15d3bf61261a25088d0c
Danny Xu <danny.d.xu@gmail.com>
2023-09-23 15:06:59 -0500

    yolo swag

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Box, extendTheme } from "@chakra-ui/react";
+import React, { useState, WheelEvent, useEffect } from "react";
+import { NextPage } from "next";
+import LandingPage from "../components/LandingPage";
+
+const HomePage: NextPage = () => {
+  return <LandingPage />;
+};
+
+export default HomePage;
