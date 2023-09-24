38604e3e38f21e0a8c35636b5ace5569ecb4c9fa
Danny Xu <danny.d.xu@gmail.com>
2023-09-23 21:40:00 -0500

    added question card

--- frontend/pages/quiz/index.tsx
+++ frontend/pages/quiz/index.tsx
@@ -1,14 +1,8 @@
-import {
-  Box,
-  Heading,
-  Text,
-  ChakraProvider,
-  extendTheme,
-  CSSReset,
-} from "@chakra-ui/react";
+import { Box, Heading, Text, VStack } from "@chakra-ui/react";
 import React from "react";
 
 import Question from "../../components/Question";
+import QuizCard from "@/components/QuestionCard";
 
 const questionData = {
   question: "What is the capital of France?",
@@ -16,7 +10,40 @@ const questionData = {
   correctAnswer: "Paris",
 };
 const TemporaryPage = () => {
-  return <Question questionData={questionData} />;
+  const dummyQuestion1 = {
+    question: "What is 2 + 2?",
+    options: [
+      { label: "3", value: "3" },
+      { label: "4", value: "4" },
+      { label: "5", value: "5" },
+      { label: "6", value: "6" },
+    ],
+  };
+
+  const dummyQuestion2 = {
+    question: "What is the capital of France?",
+    options: [
+      { label: "Berlin", value: "Berlin" },
+      { label: "Madrid", value: "Madrid" },
+      { label: "Paris", value: "Paris" },
+      { label: "Rome", value: "Rome" },
+    ],
+  };
+
+  const questionsArray = [dummyQuestion1, dummyQuestion2];
+
+  return (
+    <VStack>
+      {questionsArray.map((questionItem, index) => (
+        <QuizCard
+          key={index}
+          question={questionItem.question}
+          options={questionItem.options}
+          questionNumber={index+1}
+        />
+      ))}
+    </VStack>
+  );
 };
 
 export default TemporaryPage;
